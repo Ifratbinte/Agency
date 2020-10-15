ef40f956ef4e5cd684b9de0255cdb6c1df562301
IfratBinte <33017625+Ifratbinte@users.noreply.github.com>
2020-10-15 07:02:08 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	  $(".carousal-pro").owlCarousel({
+	    items:2,
+	    loop:true,
+	    margin:10,
+	    autoplay:true,
+	    navText:["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+	    nav:true,
+	});
+	  
+
+});
